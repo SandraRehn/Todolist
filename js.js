1a2bde2c6b5339daeecded62e366fe0079fe9b7f
Erik <erikhansson85@gmail.com>
2018-07-26 14:27:13 +0200

    texten suddas ut, vid kryssning av checkbox

--- js.js
+++ js.js
@@ -1,20 +1,46 @@
-let nyrad = document.getElementById('input');
-let checkbox = document.getElementById('checkbox');
+let input = document.getElementById('input');
+let checkbox = document.querySelector('.checkbox');
+//let checkID = document.getElementById('idcheck');
 let wrapper = document.querySelector('.wrapper');
 let list = document.querySelector('.list');
+let checkArr = [];
+checkArr.push()
 
 wrapper.addEventListener('keypress', function(e){
     let key = e.which || e.keyCode;
     console.log(e.target);
     if(key === 13){
        // let node = document.createElement("LI");
-        //let textnode = document.createTextNode("hejdu");
-        let test = `<li><input type="checkbox" id="checkbox" name="feature"/><label for="checkbox"><input type="text" name="" id="input"></label></li>`;
-       // node.appendChild(test);
-        document.querySelector('.list').appendChild(test);
+        let count = 2;
+       // let textnode = document.createTextNode("");
+        let test = 
+        `<li>
+            <input type="checkbox" id="checkbox`+count+`" name="feature" onlick = "myFunc()"/>
+                <label for="checkbox">
+                 <input type="text" name="" id="`+count+`">
+             </label>
+        </li>`;
+        
+        document.querySelector('.list').insertAdjacentHTML('beforeend', test);
+        let checkboxListener = document.getElementById('checkbox'+count);
+        (function (count){
+            checkboxListener.addEventListener('click', function(){
+                document.getElementById(count).value = '';
+            });
+        })(count);
+        count++;
 
     }
+    document.querySelector('.list:last-child').focus();
 })
+
+
+function myFunc(){
+
+    
+    
+}
+
 
 
 
